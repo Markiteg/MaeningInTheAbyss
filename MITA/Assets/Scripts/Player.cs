58b6b0ae3210b16ae11576f6be7bd62fa4ac0648
Markiteg <1233233jjjjj@gmail.com>
2023-11-18 02:09:30 +0500

    Добавил еще механик
    Добавил Дэш который работает правильно.
    Добавил приседание с проверкой можно ли встать

--- MITA/Assets/Scripts/Player.cs
+++ MITA/Assets/Scripts/Player.cs
@@ -10,18 +10,33 @@ public class Player : MonoBehaviour
     public float TimeJump;
     public float StartTimeJump;
     public float StartJumpCount;
+    public float TimeBTWDash;
+    public float StartTimeBTWDash;
+    public float TimeAFTDash;
+    public float StartTimeAFTDash;
+    public float DashSpeed;
 
     private bool IsGround = false;
     private float JumpCount = 1;
     private bool JumpControl;
+    private bool IsSide;
+    private float DashCount = 1;
+    private float StartDashCount = 1;
+    private bool IsSite;
+    private bool IsCanStay;
 
     private Rigidbody2D rb;
+    private BoxCollider2D bc;
 
     public GameObject GOisGround;
+    public GameObject GOIsCanStay;
     public LayerMask Ground;
     private void Awake()
     {
+        IsSide = true;
+        IsSite = false;
         rb = GetComponent<Rigidbody2D>();
+        bc = GetComponent<BoxCollider2D>();
     }
 
     void Update()
@@ -31,16 +46,18 @@ public class Player : MonoBehaviour
             gameObject.transform.position = new Vector2(0, 0);
         }
 
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A)) // Лево
         {
             transform.Translate(Vector3.left * speed * Time.deltaTime);
+            IsSide = true;
         }
-        else if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D)) // Право
         {
             transform.Translate(Vector3.right * speed * Time.deltaTime);
+            IsSide = false;
         }
 
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space)) // Проверка на задержку пробела
         {
             if (IsGround)
                 JumpControl = true;
@@ -64,8 +81,74 @@ public class Player : MonoBehaviour
         {
             TimeJump = StartTimeJump;
             JumpCount = StartJumpCount;
+            DashCount = StartDashCount;
+        }
+        IsGround = Physics2D.OverlapCircle(GOisGround.transform.position, 0.1f, Ground); // Проверка на земле ли игрок
+        IsCanStay = Physics2D.OverlapCircle(GOIsCanStay.transform.position, 0.5f, Ground); //Проверка может ли игрок выйти из режима приседа
+
+        if (Input.GetKeyDown(KeyCode.Q) && TimeBTWDash <= 0) //Активация Дэша
+            Dash();
+        else
+        {
+            TimeBTWDash -= Time.deltaTime;
+            if (TimeAFTDash <= 0)
+                rb.velocity = new Vector2(0, rb.velocity.y);
+            else
+                TimeAFTDash -= Time.deltaTime;
         }
 
-        IsGround = Physics2D.OverlapCircle(GOisGround.transform.position, 0.1f, Ground);
+        if (Input.GetKeyDown(KeyCode.C)) //Приседания
+        {
+            if (!IsSite)
+            {
+                IsSite = true;
+                bc.offset = new Vector2(0, -0.151821f);
+                bc.size = new Vector2(1, 1.103642f);
+            }
+            else if (!IsCanStay)
+            {
+                IsSite = false;
+                bc.offset = new Vector2(0, 0.3f);
+                bc.size = new Vector2(1, 2);
+            }
+
+        }
+    }
+
+    public void Dash() //Дэш
+    {
+        if (!IsSide)
+        {
+            if (!IsGround && DashCount > 0)
+            {
+                rb.velocity = new Vector2(DashSpeed, rb.velocity.y);
+                DashCount--;
+                TimeBTWDash = StartTimeBTWDash;
+                TimeAFTDash = StartTimeAFTDash;
+            }
+            else if (IsGround)
+            {
+                rb.velocity = new Vector2(DashSpeed, rb.velocity.y);
+                TimeBTWDash = StartTimeBTWDash;
+                TimeAFTDash = StartTimeAFTDash;
+            }
+            
+        }
+        else
+        {
+            if (!IsGround && DashCount > 0)
+            {
+                rb.velocity = new Vector2(DashSpeed * -1, rb.velocity.y);
+                DashCount--;
+                TimeBTWDash = StartTimeBTWDash;
+                TimeAFTDash = StartTimeAFTDash;
+            }
+            else if (IsGround)
+            {
+                rb.velocity = new Vector2(DashSpeed * -1, rb.velocity.y);
+                TimeBTWDash = StartTimeBTWDash;
+                TimeAFTDash = StartTimeAFTDash;
+            }
+        }
     }
 }
